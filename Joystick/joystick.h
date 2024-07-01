f6991a4d30e85153493a6c2bbc732b019fd0aa3d
zuoyanjide <zuoyanjide@163.com>
2024-07-01 16:01:28 +0800

    updata

---
+++ Joystick/joystick.h
@@ -0,0 +1,33 @@
+#ifndef _HIDUSBFX2_H_
+
+#define _HIDUSBFX2_H_
+
+#pragma warning(disable:4200)  // suppress nameless struct/union warning
+#pragma warning(disable:4201)  // suppress nameless struct/union warning
+#pragma warning(disable:4214)  // suppress bit field types other than int warning
+#include <initguid.h>
+#include <Ntddk.h>
+#include <wdm.h>
+
+#pragma warning(default:4200)
+#pragma warning(default:4201)
+#pragma warning(default:4214)
+#include <wdf.h>
+#include <wdmsec.h> // for SDDLs
+
+
+#pragma warning(disable:4201)  // suppress nameless struct/union warning
+#pragma warning(disable:4214)  // suppress bit field types other than int warning
+#include <hidport.h>
+
+#define FFB_SUPPORTED
+#define NTSTRSAFE_LIB
+#include <ntstrsafe.h>
+
+
+
+
+
+
+
+#endif   //_HIDUSBFX2_H_
